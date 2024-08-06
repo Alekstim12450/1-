17986adcf17872a7a0540529b440622124f3b3cb
Aleksander <Aleksander@test.ru>
2024-08-06 19:29:12 +0300

    обновы1

---
+++ py.py
@@ -0,0 +1,31 @@
+#  a = 3256
+#b = 5632
+#sum = a + b
+#print(sum)
+#print()
+#print("Hello, 'world' \nРад вас всех видеть")
+#print("Hissssssssssssss//////")
+
+
+# print (2+2)
+# print (9-6)
+# print (9*6)
+# print(9/6)
+
+# a = 10
+# b = 5
+# sum = a + b
+# mult = a * b
+# dif = a - b
+# dev = a / b
+# print(type(a))
+# print(type(b))
+# print (sum, mult, dif, dev)
+
+
+nanana = int(input ("Введите Ваше число:"))
+sum = nanana + 1
+print (sum)
+input ()
+
+
